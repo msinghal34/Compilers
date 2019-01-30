a07b5067f5fbac9da0f3f3590dd4e8d009cfe555
MAYANK SINGHAL <singhal@sl2-34.cse.iitb.ac.in>
2019-01-30 13:56:59 +0530

    Assignment 2 Starter Code

---
+++ Assignment 2/A2-resources/ast.hh
@@ -0,0 +1,238 @@
+
+#ifndef AST_HH
+#define AST_HH
+
+#include<string>
+#include<iostream>
+#include<iomanip>
+#include<typeinfo>
+#include<list>
+
+#define AST_SPACE "         "
+#define AST_NODE_SPACE "            "
+#define AST_SUB_NODE_SPACE "               "
+
+#define END_OF_PROG -3	
+// Used by goto_ast and basic_block
+
+using namespace std;
+
+class Ast;
+
+class Ast
+{
+protected:
+	typedef enum
+	{
+		zero_arity = 0,
+		unary_arity = 1,
+		binary_arity = 2
+	}Ast_Arity;
+
+	Data_Type node_data_type;
+	Ast_Arity ast_num_child;
+
+	int lineno;
+
+public:
+	Ast();
+	~Ast();
+
+	virtual Data_Type get_data_type();
+	virtual void set_data_type(Data_Type dt);
+
+	virtual bool is_value_zero();
+
+	virtual bool check_ast();
+	virtual Symbol_Table_Entry & get_symbol_entry();
+
+	virtual void print(ostream & file_buffer) = 0;
+	virtual void print_value(Local_Environment & eval_env, ostream & file_buffer);
+
+	virtual Eval_Result & get_value_of_evaluation(Local_Environment & eval_env);
+	virtual void set_value_of_evaluation(Local_Environment & eval_env, Eval_Result & result);
+	virtual Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer) = 0;
+
+	virtual Code_For_Ast & compile() = 0;
+	virtual Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra) = 0;
+	virtual Code_For_Ast & create_store_stmt(Register_Descriptor * store_register);
+};
+
+class Assignment_Ast:public Ast
+{
+	Ast * lhs;
+	Ast * rhs;
+
+public:
+	Assignment_Ast(Ast * temp_lhs, Ast * temp_rhs, int line);
+	~Assignment_Ast();
+
+	bool check_ast();
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class Name_Ast:public Ast
+{
+	Symbol_Table_Entry * variable_symbol_entry;
+
+public:
+	Name_Ast(string & name, Symbol_Table_Entry & var_entry, int line);
+	~Name_Ast();
+
+	Data_Type get_data_type();
+	Symbol_Table_Entry & get_symbol_entry();
+	void set_data_type(Data_Type dt);
+
+	void print(ostream & file_buffer);
+
+	void print_value(Local_Environment & eval_env, ostream & file_buffer);
+	Eval_Result & get_value_of_evaluation(Local_Environment & eval_env);
+	void set_value_of_evaluation(Local_Environment & eval_env, Eval_Result & result);
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+	Code_For_Ast & create_store_stmt(Register_Descriptor * store_register);
+};
+
+template <class T>
+class Number_Ast:public Ast
+{
+	T constant;
+
+public:
+	Number_Ast(T number, Data_Type constant_data_type, int line);
+	~Number_Ast();
+
+	Data_Type get_data_type();
+	void set_data_type(Data_Type dt);
+	bool is_value_zero();
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class Arithmetic_Expr_Ast:public Ast
+{
+protected:
+	Ast * lhs;
+	Ast * rhs;
+
+public:
+	Arithmetic_Expr_Ast() {}
+	~Arithmetic_Expr_Ast() {}
+
+	Data_Type get_data_type();
+	void set_data_type(Data_Type dt);
+	bool check_ast();
+
+	virtual void print(ostream & file_buffer) = 0;
+	
+	virtual Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer) = 0;
+
+	// compile
+	virtual Code_For_Ast & compile() = 0;
+	virtual Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra) = 0;
+};
+
+class Plus_Ast:public Arithmetic_Expr_Ast
+{
+public:
+	Plus_Ast(Ast * l, Ast * r, int line);
+	~Plus_Ast() {}
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	// compile
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class Minus_Ast:public Arithmetic_Expr_Ast
+{
+public:
+	Minus_Ast(Ast * l, Ast * r, int line);
+	~Minus_Ast() {}
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	// compile
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class Divide_Ast:public Arithmetic_Expr_Ast
+{
+public:
+	Divide_Ast(Ast * l, Ast * r, int line);
+	~Divide_Ast() {}
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	// compile
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class Mult_Ast:public Arithmetic_Expr_Ast
+{
+public:
+	Mult_Ast(Ast * l, Ast * r, int line);
+	~Mult_Ast() {}
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	// compile
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class UMinus_Ast:public Arithmetic_Expr_Ast
+{
+public:
+	UMinus_Ast(Ast * l, Ast * r, int line);
+	~UMinus_Ast() {}
+	
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	// compile
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+class Return_Ast:public Ast		// Not required in A2
+{
+
+public:
+	Return_Ast(int line);
+	~Return_Ast();
+
+	void print(ostream & file_buffer);
+
+	Eval_Result & evaluate(Local_Environment & eval_env, ostream & file_buffer);
+
+	Code_For_Ast & compile();
+	Code_For_Ast & compile_and_optimize_ast(Lra_Outcome & lra);
+};
+
+#endif
